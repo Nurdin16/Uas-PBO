c3e7c701765cae9f64af8feaac6de9760883f9e6
nurdin kurniawan <132554292+Nurdin16@users.noreply.github.com>
2023-07-12 16:57:42 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String [] args) {
+        
+        Buku buku1 = new Buku("0001", "Pemrogramman Java");
+        Buku buku2 = new Buku("0002", "Pemrogramman C++");
+        BukuLuar buku3 = new BukuLuar("0003", "Pemrogramman Python");
+    
+        Petugas petugas1 = new Petugas("Alex", "P001");
+        Peminjam peminjam1 = new Mahasiswa();
+        peminjam1.setNamaPeminjam("Huda");
+        peminjam1.setKodePeminjam("M001");
+
+        
+        
+       peminjam1.doLogin();
+       //peminjam1.doLogout();
+
+        try{
+        Peminjaman peminjaman1 = new Peminjaman(peminjam1, petugas1, buku3);
+
+        System.out.println("Nama Peminjam :"+peminjaman1.getPeminjam().getNamaPeminjam());
+        System.out.println("Kode Peminjam :"+peminjaman1.getPeminjam().getKodePeminjam());
+        System.out.println("Nama Petugas :"+peminjaman1.getPetugas().getNamaPetugas());
+        System.out.println("Nama Buku :"+peminjaman1.getBuku().getNamaBuku());
+        Denda denda1 = new Denda(peminjaman1, 6);
+        System.out.println("Total Denda :"+denda1.hitungDenda());
+        
+
+        System.out.println("=====================================");
+
+        Peminjaman peminjaman2 = new Peminjaman(peminjam1, petugas1, buku2);
+
+        System.out.println("Nama Peminjam :"+peminjaman2.getPeminjam().getNamaPeminjam());
+        System.out.println("Kode Peminjam :"+peminjaman2.getPeminjam().getKodePeminjam());
+        System.out.println("Nama Petugas :"+peminjaman2.getPetugas().getNamaPetugas());
+        System.out.println("Nama Buku :"+peminjaman2.getBuku().getNamaBuku());
+        Denda denda2 = new Denda(peminjaman2, 7);
+        System.out.println("Total Denda :"+denda2.hitungDenda());
+        
+
+        
+
+        }catch (Exception e){
+            System.out.println("Terjadi Kesalahan"+e.getMessage());
+        } finally {
+            System.out.println("Program Selesai");
+        }
+
+        
+    }
+}
---
+++ denda.java
@@ -0,0 +1,45 @@
+class Denda {
+    private Peminjaman peminjaman;
+    private int jumlahDenda = 10000;
+    private int HariTerlambat;
+    private int TotalDenda;
+
+    public Denda(Peminjaman peminjaman, int HariTerlambat ) {
+        this.peminjaman = peminjaman;
+        this.HariTerlambat = HariTerlambat;
+    }
+
+    public Peminjaman getPeminjaman() {
+        return peminjaman;
+    }
+
+    public void setPeminjaman(Peminjaman peminjaman) {
+        this.peminjaman = peminjaman;
+    }
+    public int getJumlahDenda() {
+        return jumlahDenda;
+    }
+
+    public void setJumlahDenda(int jumlahDenda) {
+        this.jumlahDenda = jumlahDenda;
+    }
+    public int getHariTerlambat() {
+        return HariTerlambat;
+    }
+    public void setHariTerlambat(int HariTerlambat) {
+        this.HariTerlambat = HariTerlambat;
+    }
+    public int getTotalDenda() {
+        return TotalDenda;
+    }
+    public void setTotalDenda(int totalDenda) {
+        this.TotalDenda = totalDenda;
+    }
+    public int hitungDenda(){
+        return this.HariTerlambat * this.jumlahDenda;
+    }
+
+
+
+
+}
